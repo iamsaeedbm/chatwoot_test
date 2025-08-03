f79dae42226c1d036e2f0c9f0b5aad260c2ba74e
saeedbm <iamsaeedbm@gmail.com>
2025-08-03 23:43:57 +0330

    make clean structure

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(child: Column(children: [])),
+      ),
+    );
   }
 }
