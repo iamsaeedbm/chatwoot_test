eb49902172a0699edf155fbc2870e94c3a65b1d5
saeedbm <iamsaeedbm@gmail.com>
2025-08-05 22:58:09 +0330

    add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chatwoot_test/layers/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(child: Column(children: [])),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
